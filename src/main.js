19546894b077e3cb7f72604aee4c3a555850e1d8
William Cameron <william@williamcameron.co.uk>
2019-10-17 23:51:51 +0100

    add vuetify calendar

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+
+Vue.use(VueTextareaAutosize);
 
 Vue.config.productionTip = false
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBtDf0M4d6DCLLXsZ7BRlc4hzQHHI0kGZc",
+    authDomain: "vuetify-calendar.firebaseapp.com",
+    databaseURL: "https://vuetify-calendar.firebaseio.com",
+    projectId: "vuetify-calendar",
+    storageBucket: "vuetify-calendar.appspot.com",
+    messagingSenderId: "205712808747",
+    appId: "1:205712808747:web:d2ef315814ebaa966a036e"
+})
+
+export const db = firebase.firestore();
 
 new Vue({
   vuetify,
